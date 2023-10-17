9bfcad350164e7e33f017823dfb1d736b5c8681c
Michael Bradshaw <michael.bradshaw@centre.edu>
2023-10-17 07:52:36 -0400

    added lab files

---
+++ src/main/java/Queue.java
@@ -0,0 +1,42 @@
+public class Queue<T>
+{
+    //be sure that your attributes are protected, so subclasses can use them
+
+
+   public Queue()
+   {
+   
+   }
+
+
+    /**Adds val to the end of the queue
+     */
+   public void push(T val)
+   {
+   
+   }
+
+
+    /**
+       removes and returns the oldest value in the queue
+       throws QueueUnderFlowException if the queue is empty
+     */
+   public T pop()
+   {
+       return null;
+   }    
+
+
+    /**
+      returns true if the queue is empty
+     */
+
+   public boolean isEmpty()
+   {
+       return false;
+   }
+
+
+
+
+}
